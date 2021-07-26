3ab711c7b3f9deee4d3bdcc2ae7a57178b57dc76
Knuth Konrad <Knuth.Konrad@BasicAware.de>
2021-07-26 12:22:59 +0200

    WIP: Command line parameter parsing.

--- libBAUtilCoreCS/CmdArgs.cs
+++ libBAUtilCoreCS/CmdArgs.cs
@@ -2,11 +2,204 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using static libBAUtilCoreCS.StringUtil;
+
 namespace libBAUtilCoreCS.Utils.Args
 {
+
+   /// <summary>
+   /// Commandline parameter handling
+   /// See https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/march/net-parse-the-command-line-with-system-commandline
+   /// See https://github.com/commandlineparser/commandline
+   /// </summary>
    class CmdArgs
    {
-   }
+
+      #region "Declarations
+
+      public enum eArgumentDelimiterStyle { Windows, POSIX };
+
+
+      // Default arguments and key/value delimiter
+      private const string DELIMITER_ARGS_WIN = "/";
+      private const string DELIMITER_ARGS_POSIX = "--";
+      private const string DELIMITER_VALUE = "=";
+
+      Boolean mbolCaseSensitive;  // Treat parameter names as case-sensitive?
+
+      private string msDelimiterArgs = String.Empty;  // Arguments delimiter, typically "/"
+      private string msDelimiterValue = String.Empty; // Key/value delimiter, typically "="
+      private List<string> listValidParameters;       // List of all valid parameter
+
+      private List<KeyValue> mcolKeyValues;
+
+      #endregion
+
+      #region "Properties - Public"
+      
+      public Int32 ParametersCount
+      {
+         get
+         {
+            if (KeyValues != null)
+            {
+               return KeyValues.Count;
+            }
+            else
+            {
+               return 0;
+            }
+         }
+      }
+
+      public Boolean CaseSensitive
+      {
+         get { return mbolCaseSensitive; }
+         set { mbolCaseSensitive = value; }
+      }
+
+      public string DelimiterArgs
+      {
+         get { return msDelimiterArgs; }
+         set { msDelimiterArgs = value; }
+      }
+
+      public string DelimiterValue
+      {
+         get { return msDelimiterValue; }
+         set { msDelimiterValue = value; }
+      }
+
+      public List<string> ValidParameters
+      {
+         get { return listValidParameters; }
+         set { listValidParameters = value; }
+      }
+
+      public List<KeyValue> KeyValues
+      {
+         get { return mcolKeyValues; }
+         set { mcolKeyValues = value; }
+      }
+
+      #endregion
+
+      #region "Methods - Private"
+      
+      /// <summary>
+      /// Is a parameter present more than once?
+      /// </summary>
+      private Boolean HasDuplicate(KeyValue currentKey, Int32 currentIndex)
+      {
+
+         // Safe guard
+         if (KeyValues.Count < 1) { return false; }
+
+         // Compare the passed parameter to the list and see if there's a duplicate entry
+         for (Int32 i = 0;  i<= KeyValues.Count - 1; i++)
+         {
+            KeyValue o = KeyValues[i];
+
+            if (CaseSensitive == false)
+            {
+               if ((o.Key.ToLower() == currentKey.Key.ToLower()) && (i != currentIndex))
+               {
+                  return true;
+               }
+            }
+            else
+            {
+               if ((o.Key == currentKey.Key) && (i != currentIndex))
+               {
+                  return true;
+               }
+            }
+
+         }  // for (Int32 i = 0;  i<= KeyValues.Count - 1; i++)
+
+            // Reaching here, we've found no duplicates
+            return false;
+      }
+
+      private Boolean ParseCmd(string[] asArgs, Int32 startIndex = 0)
+      {
+
+         string sKey = String.Empty;
+         string sValue = String.Empty;
+         Boolean bolResult = true;
+
+         for (Int32 i = startIndex; i<=  asArgs.Length - 1; i++)
+         {
+            bolResult = bolResult && ParseParam(asArgs[i]);
+         }
+
+         return bolResult;
+
+      }
+
+
+
+      /// <summary>
+      /// Parses a single key/pair combo into a matching <see cref="KeyValue"/> object
+      /// </summary>
+      /// <param name="sParam"></param>
+      /// <returns></returns>
+      private Boolean ParseParam(string sParam)
+      {
+
+
+         if (sParam.Length < 1)
+         {
+            return true;
+         }
+
+         if (sParam.Contains(DelimiterValue))
+         {
+
+            // Parameter of the form /key=value
+
+            KeyValue o = new KeyValue(sParam);
+
+            // '/file' for /file=MyFile.txt
+            KeyLong = Left(sParam, InStr(sParam, DelimiterValue) - 1).Trim
+            // Remove the leading delimiter, results in 'file'
+            If.KeyLong.IndexOf(Me.DelimiterArgs) > -1 Then
+               .KeyLong = Mid(.KeyLong, Me.DelimiterArgs.Length + 1)
+            End If
+            // Since we parse this from the command line, set both
+            .KeyShort = .KeyLong
+            .Value = Mid(sParam, InStr(sParam, DelimiterValue) + 1)
+
+            .KeyValues.Add(o)
+
+         Else
+            ' Parameter of the form /Value.
+            ' These are considered to be boolean parameters. If present, their value is 'True'
+
+            Dim o As New KeyValue(sParam)
+
+            With o
+               .KeyLong = sParam.Trim
+               If .KeyLong.IndexOf(Me.DelimiterArgs) > -1 Then
+                  .KeyLong = Mid(.KeyLong, Me.DelimiterArgs.Length + 1)
+               End If
+               ' Since we parse this from the command line, set both
+               .KeyShort = .KeyLong
+               .Value = True
+            End With
+
+            .KeyValues.Add(o)
+
+         End If
+
+               return true;
+
+      }
+
+
+      #endregion
+
+   }  // CmdArgs
 
    class KeyValue
    {
@@ -73,7 +266,6 @@ namespace libBAUtilCoreCS.Utils.Args
          set { msOriginalParameter = value};
       }
 
-
       /// <summary>
       /// Parameter value, e.g. 'MyFile.txt' from /file=MyFile.txt
       /// </summary>
@@ -98,30 +290,22 @@ namespace libBAUtilCoreCS.Utils.Args
          }
       }
 
-   #endregion
+         #endregion
 
-   public void CmdArgs(string originalParam, string keyShort = "",
-                     string keyLong = "", Object value = null,
-                     string helpText = "")
+
+      public KeyValue(string originalParam, string keyShort = "",
+                        string keyLong = "", Object value = null,
+                        string helpText = "")
       {
          // ToDo: base bzw.MyBase needed in C# constructor
 
-         /*
-         MyBase.New
-
-            With Me
-               .HelpText = helpText
-               .KeyLong = keyLong
-               .KeyShort = keyShort
-               .OriginalParameter = originalParam
-               .Value = value
-            End With
-
-         End Sub
-         */
+         HelpText = helpText;
+         KeyLong = keyLong;
+         KeyShort = keyShort;
+         OriginalParameter = originalParam;
+         Value = value;
       }
 
 
-
+      }
    }
-}
